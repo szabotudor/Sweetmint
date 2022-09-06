7008c59c0a46d7d5a84e8e872d0beb43569928ae
szabotudor <szabo.tudor@gmail.com>
2022-09-07 00:07:52 +0200

    Moved main cpp file to src folder

--- cmake/SourcesConfig.cmake
+++ cmake/SourcesConfig.cmake
@@ -1,6 +1,5 @@
 file(GLOB_RECURSE SOURCES "${CMAKE_SOURCE_DIR}/src/*.cpp")
 file(GLOB_RECURSE SOURCES_C "${CMAKE_SOURCE_DIR}/src/*.c")
-list(APPEND SOURCES "${CMAKE_SOURCE_DIR}/Sweetmint.cpp")
 list(APPEND SOURCES ${SOURCES_C})
 list(APPEND SOURCES $<TARGET_OBJECTS:glfw>)
 list(APPEND SOURCES "include/Sweetmint.hpp")
