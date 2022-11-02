f91ada061d8cd4f2fb9db616cd3653e98e37fe56
Tudor Szabo <tudor@laptop-tudor>
2022-11-02 15:16:11 +0100

    Fixed typo in comment

--- BuildSettings.cmake
+++ BuildSettings.cmake
@@ -1,4 +1,4 @@
-#Edit this file to change settings for build settings
+#Edit this file to change build settings
 
 #Set this to 'ON' to build the shared version of the library
 set(SWEETMINT_SHARED OFF)
